0036dd962882fa636c0f22761263bdf801ad8d4d
ismael <shalanga@gmail.com>
2015-07-28 20:12:52 -0300

    moved!

--- kafe.real.js
+++
@@ -1,9 +0,0 @@
-/**
-*	v.1.0
-*/
-
-// http://blog.tompawlak.org/number-currency-formatting-javascript
-function _format(num)
-{
-    return num.toFixed(2).replace(".", ",").replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
-}
